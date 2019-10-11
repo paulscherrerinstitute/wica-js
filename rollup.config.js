81ed607c45448c9dd0986b542f87aec97f0fa3c2
rees_s <simon.rees@psi.ch>
2019-10-11 14:00:30 +0200

    Suppressed terse output to assist in evl debugging.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import { terser } from 'rollup-plugin-terser';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import copy from "rollup-plugin-copy-assets";
 
 export default {
@@ -16,8 +18,12 @@ export default {
                 "src/wica.css",
             ],
         }),
+        resolve(),
+        commonjs(),
         // Invoke terser but just use the default options until
         // it is proved that we need something else.
-        terser(),
+        terser( {
+            exclude: [ '*' ]
+        }),
     ]
 };
