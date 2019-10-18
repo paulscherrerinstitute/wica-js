83cf3d6e8ca45d63b6941156bb5f3b5668482457
rees_s <simon.rees@psi.ch>
2019-10-18 23:44:51 +0200

    Trigger ws-1.1.3-dev release.

--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,45 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import copy from "rollup-plugin-copy-assets";
 
-export default {
-    input: 'src/wica.js',
-    output: {
-        file: 'dist/rel/wica.js',
-        format: 'esm',
-        beautify: true,
-        sourcemap: true,
-    },
-    plugins: [
-        copy({
-            assets: [
-                "src/about.html",
-                "src/wica.css",
-            ],
-        }),
-        resolve(),
-        commonjs(),
-        // Invoke terser but just use the default options until
-        // it is proved that we need something else.
-        terser( {
-            exclude: [ '*' ]
-        }),
-    ]
-};
+export default [ {
+        input: 'src/wica.js',
+        output: {
+            dir: 'dist/rel',
+            format: 'esm',
+            beautify: true,
+            sourcemap: true,
+        },
+        plugins: [
+            copy({
+                assets: [
+                    "src/about.html",
+                    "src/wica.css",
+                ],
+            }),
+            resolve(),
+            commonjs(),
+            // Invoke terser but just use the default options until
+            // it is proved that we need something else.
+            terser( {
+                exclude: [ '*' ]
+            }),
+        ]
+    }, {
+        input: 'src/client-api.js',
+        output: {
+            dir: 'dist/rel',
+            format: 'esm',
+            beautify: true,
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            // Invoke terser but just use the default options until
+            // it is proved that we need something else.
+            terser({
+                exclude: ['*']
+            }),
+        ]
+    }
+];
