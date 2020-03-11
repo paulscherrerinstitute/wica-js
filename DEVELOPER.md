e59c04c2e78e9a9dea0e29cf5a4c7abf0ef680a7
rees_s <simon.rees@psi.ch>
2020-03-11 20:43:06 +0100

    #13 ENHANCEMENT: Add support for defining Wica-JS library startup behaviour in script tag.

--- DEVELOPER.md
+++ DEVELOPER.md
@@ -2,6 +2,64 @@
 
 This page is a miscellaneous collection of notes intended to assist software developers.
 
+## Additional Wica Library Load Options
+
+In a normal Wica HTML page the wica library is typically loaded by including a script element of the 
+form shown below:
+```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8"/>
+   <script src="/wica/wica.js" type="module"></script>
+   ...
+</head>
+</html>
+```
+Wica now supports the possibility of overriding the default library behaviour after load.
+This is achieved through the use of HTML data-* attributes which can be defined in the 
+script element. Thus:
+```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8"/>
+   <script src="/wica/wica.js" data-wicaLoadOption=XXXX type="module"></script>
+   ...
+</head>
+</html>
+```
+
+The following load options are currently supported:
+
+### data-wica-log-level=N
+
+This allows the Wica library console logging level to be set. 
+See the [API logger documentation](https://paulscherrerinstitute.github.io/wica-js/latest/module-logger.html)
+for the supported values and the current default.
+
+Example: Set the log level to INFO level.
+```
+   ...
+   <script src="/wica/wica.js" data-wica-log-level=3 type="module"></script>
+   ...
+```
+
+### data-wica-stream-server-url=URL
+
+This allows the Wica-JS library to send requests to a wica stream server other than the server which 
+was the origin of the user's web page.
+
+This facilitates debugging applications on a local web server whilst obtaining
+live data from the control system from within the user's facility.
+
+Example: Target requests to the PSI's Wica Development Server.
+```
+   ...
+   <script src="/wica/wica.js" data-wica-stream-server-url="https://gfa-wica-dev.psi.ch" type="module"></script>
+   ...
+```
+
 ## How to Make a New Software Release
 
 1. Use your favourite editor to set the project version string in the 'package.json' 
--- src/wica.js
+++ src/wica.js
@@ -1,5 +1,5 @@
 /**
- * Provides the main entry point for supporting a  Wica-aware document.
+ * Provides the main entry point for supporting a Wica-aware document.
  *
  * @module
  */
@@ -13,17 +13,50 @@ export { DocumentSupportLoader, StreamManager, PlotBuffer }
 
 /*- Script Execution Starts Here ---------------------------------------------*/
 
-// Configure the logging level required for this application.
-log.setLevel( log.logLevels.WARN );
-
 log.info( "Wica is loading support for the current document... ");
+
+// Set up a variable that points to the script tag that was used
+// to load wica. This can then be parsed to extract the HTML5 data-*
+// options which specify the library's behaviour.
+function getWicaScriptElement()
+{
+    // The algorithm below was inspired by the Stack Exchange discussion here:
+    // https://stackoverflow.com/questions/403967/how-may-i-reference-the-script-tag-that-loaded-the-currently-executing-script
+    const scripts = document.querySelectorAll( "script[src*= 'wica.js' ]" );
+    if ( scripts.length === 1 ) {
+        return scripts[ 0 ];
+    }
+    else {
+        return null;
+    }
+}
+const thisScriptEle = getWicaScriptElement();
+if ( thisScriptEle === null ) {
+    log.error( "The wica library could not be loaded since the wica script element could not be found." );
+}
+
+// Configure the logging level that will be used for this library. This can be
+// overriden externally by loading the wica library with an HTML script tag
+// that looks as follows:
+// <script data-wica-log-level= src="wica/wica.js" type="module"></script>
+const logLevel = thisScriptEle.hasAttribute( "data-wica-log-level" ) ?
+    thisScriptEle.getAttribute("data-wica-log-level" ) :  log.logLevels.DEFAULT;
+log.setLevel( logLevel );
+log.info( "The configured log level has been set to: ", logLevel );
 
 // Define the server this application is intended to target.
 const WICA_OWN_HOST = location.origin;
 
-// Create and activate a document support loader for the document
-// which loads this library.
-const documentSupportLoader = new DocumentSupportLoader( WICA_OWN_HOST );
+// Configure the URL of the stream server that will be targeted by this library.
+// This can be overriden externally by loading the wica library with an HTML
+// script tag that looks as follows:
+// <script data-wica-stream-server-url="https://gfa-wica.psi.ch" src="wica/wica.js" type="module"></script>
+const streamServerUrl = thisScriptEle.hasAttribute( "data-wica-stream-server-url" ) ?
+    thisScriptEle.getAttribute("data-wica-stream-server-url" ) : WICA_OWN_HOST;
+const documentSupportLoader = new DocumentSupportLoader( streamServerUrl );
+log.info( "The wica library will communicate with the wica stream server at the following URL: ", streamServerUrl );
+
+// Create and activate a document support loader for the document which loaded this library.
 documentSupportLoader.activate( 200, 200 );
 
 // Attach a handler to shut things down when the browser navigates away.
@@ -41,7 +74,7 @@ function restartDocumentSupportLoader() {
     documentSupportLoader.activate( 200, 200 );
     log.info( "Wica document support loader was activated OK." );
 }
+
 document.wicaRestartDocumentSupportLoader = restartDocumentSupportLoader;
 
 log.info( "Wica support loaded OK. ");
-
