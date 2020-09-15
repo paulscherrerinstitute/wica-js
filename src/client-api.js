c0a78c55b083abaad9d80a8e765bc66a64261270
rees_s <simon.rees@psi.ch>
2020-09-15 10:16:03 +0100

    Issue #27. Create release 1.3.0. Update documentation.

--- src/client-api.js
+++ src/client-api.js
@@ -1,18 +1,17 @@
 /**
- * Provides the entrypoint API for leveraging all functionality associated with Wica
- * from the Javascript client side.
+ * Provides an API for leveraging Wica functionality from the Javascript client side.
  * @module
  */
 
 /*- Import/Export Declarations -----------------------------------------------*/
 
 import * as log from "./logger.js"
-import {DocumentSupportLoader} from "./document-support-loader.js";
-import {StreamManager} from "./stream-manager.js";
+
 import {PlotBuffer} from "./plot-buffer.js";
+import {StreamManager} from "./stream-manager.js";
+import {DocumentSupportLoader} from "./document-support-loader.js";
 
 export {
-
     /**
      * Class providing a facility for buffering the received information from one
      * or more wica-aware elements, subsequently making it available to third-parties
@@ -20,19 +19,23 @@ export {
      * See {@link module:plot-buffer~PlotBuffer PlotBuffer}.
      */
     PlotBuffer,
+}
 
+export {
     /**
      * Class providing support for creating a new WicaStream on the Wica server, for
      * subscribing to it and for publishing locally the received information.
      * See {@link module:stream-manager~StreamManager StreamManager}.
      */
-    StreamManager,
+        StreamManager,
+}
 
+export {
     /**
      * Class providing all the functionality necessary to support a wica-aware html page.
      * See {@link module:document-support-loader~DocumentSupportLoader DocumentSupportLoader}.
      */
-    DocumentSupportLoader
+        DocumentSupportLoader
 }
 
 /*- Script Execution Starts Here ---------------------------------------------*/
