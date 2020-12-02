afd6816484c7c4ec6c1d7321bd3b387afd606a10
rees_s <simon.rees@psi.ch>
2020-12-03 00:37:45 +0100

    Added hints on how to use log levels in this library.

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,34 @@
 /**
  * Provides wica logging support.
  *
+ * Below are some suggestions from Stack Overflow on how to effectively use these levels effectively.
+ * Credit: https://stackoverflow.com/questions/2031163/when-to-use-the-different-log-levels
+ *
+ * <ul>
+ *     <li>Trace - Only when I would be "tracing" the code and trying to find one part of a function specifically.</li>
+ *
+ *     <li>Debug - Information that is diagnostically helpful to people more than just developers (IT, sysadmins,
+ *     etc.).</li>
+ *
+ *     <li>Info - Generally useful information to log (service start/stop, configuration assumptions, etc). Info I
+ *     want to always have available but usually don't care about under normal circumstances. This is my out-of-the-box
+ *     config level..</li>
+ *
+ *     <li>Warn - Anything that can potentially cause application oddities, but for which I am automatically recovering.
+ *     (Such as switching from a primary to backup server, retrying an operation, missing secondary data, etc.).</li>
+ *
+ *     <li>Error - Any error which is fatal to the operation, but not the service or application (can't open a
+ *     required file, missing data, etc.). These errors will force user (administrator, or direct user) intervention.
+ *     These are usually reserved (in my apps) for incorrect connection strings, missing services, etc.</li>
+ *
+ *     <li>Fatal - Any error that is forcing a shutdown of the service or application to prevent data loss (or
+ *     further data loss). I reserve these only for the most heinous errors and situations where there is
+ *     guaranteed to have been data corruption or loss.</li>
+ *
+ * Note: the fatal function is not currently implemented.
+ * </ul>
+ *
+ * @static
  * @module
  */
 
@@ -13,7 +41,7 @@ export { logLevels, setLevel, trace, debug, log, info, warn, error };
 
 /**
  * JS Object that defines the available log levels including the default that
- * will be used in the absence of an explict call to the setLevel function.
+ * will be used in the absence of an explicit call to the setLevel function.
  *
  * @property {number} [NONE = 0]
  * @property {number} [ERROR = 1]
